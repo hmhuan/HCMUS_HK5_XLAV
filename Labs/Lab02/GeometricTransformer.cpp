28706a8dee40dbe7ab63465e3e704d7628ec82ad
hmhuan <minhhuanhuynh289@gmail.com>
2018-11-12 16:22:32 +0700

    zoom done, rotates have problems

--- Labs/Lab02/GeometricTransformer.cpp
+++ Labs/Lab02/GeometricTransformer.cpp
@@ -13,6 +13,14 @@ PixelInterpolate::~PixelInterpolate()
 
 void BilinearInterpolate::Interpolate(float tx, float ty, uchar * pSrc, int srcWidthStep, int nChannels, uchar * pDstRow)
 {
+	int l = round(tx), r = round(ty);
+	float a = tx - l, b = ty - r;
+	uchar * pSrcRow0 = pSrc + (l * srcWidthStep + r * nChannels); // f(l, r)
+	uchar * pSrcRow1 = pSrc + ((l + 1) * srcWidthStep + r * nChannels); // f(l + 1, r)
+	uchar * pSrcRow2 = pSrc + (l * srcWidthStep + (r + 1) * nChannels); // f (l, r + 1)
+	uchar * pSrcRow3 = pSrc + ((l+1) * srcWidthStep + (r+1) * nChannels); // f(l + 1, r + 1)
+	for (int i = 0; i < nChannels; i++)
+		pDstRow[i] = saturate_cast<uchar>((1 - a)*(1 - b)*pSrcRow0[i] + a*(1 - b)*pSrcRow1[i] + b*(1 - a)*pSrcRow2[i] + a*b*pSrcRow3[i]);
 }
 
 BilinearInterpolate::BilinearInterpolate()
@@ -27,7 +35,11 @@ BilinearInterpolate::~BilinearInterpolate()
 
 void NearestNeighborInterpolate::Interpolate(float tx, float ty, uchar * pSrc, int srcWidthStep, int nChannels, uchar * pDstRow)
 {
-
+	int x =round(tx), y = round(ty);
+	uchar * pSrcRow = pSrc + (x * srcWidthStep + y * nChannels);
+	
+	for (int i = 0; i < nChannels; i++)
+		pDstRow[i] = pSrcRow[i];
 }
 
 NearestNeighborInterpolate::NearestNeighborInterpolate()
@@ -43,14 +55,32 @@ NearestNeighborInterpolate::~NearestNeighborInterpolate()
 void AffineTransform::Translate(float dx, float dy)
 {
 	//Gán các giá trị dx, dy tại (0, 2), (1, 2)
-	_matrixTransform.at<float>(0, 2) = dx;
-	_matrixTransform.at<float>(1, 2) = dy;
-	for (int i = 0; i < 3; i++) {
+	Mat temp;
+	temp.create(3, 3, CV_32FC1);
+	//Khởi tạo matrix 3x3 với [1, 0, 0]
+	//						  [0, 1, 0]
+	//						  [0, 0, 1]	
+	int width = temp.cols, height = temp.rows;
+	for (int y = 0; y < height; y++)
+		for (int x = 0; x < width; x++)
+			if (x == y)
+				temp.at<float>(x, y) = 1;
+			else
+				temp.at<float>(x, y) = 0;
+	
+	temp.at<float>(0, 2) = dx;
+	temp.at<float>(1, 2) = dy;
+
+	_matrixTransform = _matrixTransform * temp;
+	
+	for (int i = 0; i < 3; i++)
+	{
 		for (int j = 0; j < 3; j++)
 			std::cout << _matrixTransform.at<float>(i, j) << " ";
 		std::cout << "\n";
 	}
 	std::cout << "\n";
+
 }
 
 void AffineTransform::Rotate(float angle)
@@ -63,10 +93,12 @@ void AffineTransform::Rotate(float angle)
 	_matrixTransform.at<float>(0, 1) = -sinAlpha;
 	_matrixTransform.at<float>(1, 0) = sinAlpha;
 	_matrixTransform.at<float>(1, 1) = cosAlpha;
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++)
-			std::cout << _matrixTransform.at<float>(i, j) << " ";
-		std::cout << "\n";
+
+	for (int i = 0; i < 3; i++)
+	{
+	for (int j = 0; j < 3; j++)
+	std::cout << _matrixTransform.at<float>(i, j) << " ";
+	std::cout << "\n";
 	}
 	std::cout << "\n";
 }
@@ -76,15 +108,8 @@ void AffineTransform::Scale(float sx, float sy)
 	//Gán các giá trị sx, sy tại (0, 0), (1, 1)
 	_matrixTransform.at<float>(0, 0) = sx;
 	_matrixTransform.at<float>(1, 1) = sy;
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++)
-			std::cout << _matrixTransform.at<float>(i, j) << " ";
-		std::cout << "\n";
-	}
-	std::cout << "\n";
 }
 		
-
 void AffineTransform::TransformPoint(float & x, float & y)
 {
 	float P[3] = { x, y, 1 }, P1[3] = {0, 0, 0}; //Tạo 2 ma trận điểm ảnh nguồn và đích
@@ -122,23 +147,97 @@ AffineTransform::~AffineTransform()
 
 int GeometricTransformer::Transform(const Mat & beforeImage, Mat & afterImage, AffineTransform * transformer, PixelInterpolate * interpolator)
 {
-	return 0;
+	//width là chiều rộng ảnh đích, height là chiều cao ảnh đích.
+	//swidth là chiều rộng ảnh nguồn, height là chiều cao ảnh nguồn.
+	int width = afterImage.cols, height = afterImage.rows;
+	int swidth = beforeImage.cols, sheight = beforeImage.rows;
+	//nChannels là số kênh màu
+	int nChannels = beforeImage.channels();
+	//swidthStep là khoảng cách tính theo byte giữa 2 pixel cùng cột trên 2 dòng kế tiếp của ảnh nguồn
+	//widthStep là khoảng cách tính theo byte giữa 2 pixel cùng cột trên 2 dòng kế tiếp của ảnh đích
+	int swidthStep = beforeImage.step[0];
+	int widthStep = afterImage.step[0];
+	//pData là con trỏ quản lý vùng nhớ ảnh đích
+	uchar* pData = (uchar*)afterImage.data; 
+	// psData là con trỏ quản lý vùng nhớ ảnh nguồn
+	uchar* psData = (uchar*)beforeImage.data;
+	float x, y;
+	for (int i = 0; i < height; i++, pData += widthStep) 
+	{
+		uchar * pRow = pData; //Con trỏ dòng của ảnh đích
+		for (int j = 0; j < width; j++, pRow += nChannels) 
+		{
+			x = i; y = j;
+			transformer->TransformPoint(x, y); //Tính ra giá trị của p
+			if (round(x) < sheight && round(y) < swidth && x >= 0 && y >= 0)
+				interpolator->Interpolate(x, y, psData, swidthStep, nChannels, pRow);
+			else
+			{
+				for (int k = 0; k < nChannels; k++)
+					pRow[k] = 0;
+			}
+
+		}
+	}
+
+	return 1;
 }
 
 int GeometricTransformer::RotateKeepImage(const Mat & srcImage, Mat & dstImage, float angle, PixelInterpolate * interpolator)
 {
-	return 0;
+	Mat temp;
+	//Khởi tạo đối tượng affineTf và gọi phương thức Rotate
+	AffineTransform * affineTf = new AffineTransform();
+	AffineTransform * affineTf1 = new AffineTransform();
+	
+	float angle1 = angle * M_PI / 180;
+	int width = srcImage.cols, height = srcImage.rows;
+	//int dstWidth = width * fabs(cosf(angle1)) + height * fabs(sinf(angle1));
+	//int dstHeight = width * fabs(sinf(angle1)) + height * fabs(cosf(angle1));
+
+	int dstWidth = height * 2;
+	int dstHeight = width * 2;
+	
+	//Khởi tạo ảnh đích có kích thước và type giống ảnh nguồn
+	temp.create(height * 2, width * 2, srcImage.type()); ///edit ????
+	dstImage.create(height * 2, width * 2, srcImage.type());
+	
+	affineTf1->Translate(-height / 2, -width / 2);
+	Transform(srcImage, temp, affineTf1, interpolator);
+	imshow("Temp", temp);
+	//waitKey(0);
+	affineTf->Rotate(-angle);
+	//affineTf->Translate(-fabs((dstHeight*sinf(angle1) - dstWidth*cosf(angle1))/2), 0); ///???
+	affineTf->Translate(-height * sinf(angle1), 0);
+	Transform(srcImage, dstImage, affineTf, interpolator);
+	return 1;
 }
 
 int GeometricTransformer::RotateUnkeepImage(const Mat & srcImage, Mat & dstImage, float angle, PixelInterpolate * interpolator)
 {
-	return 0;
+	AffineTransform * affineTf = new AffineTransform();
+	AffineTransform * affineTf1 = new AffineTransform();
+	Mat temp;
+	//Khởi tạo ảnh đích có kích thước và type giống ảnh nguồn
+	
+	dstImage.create(srcImage.rows,srcImage.cols, srcImage.type());
+
+	affineTf->Rotate(-angle);
+	affineTf->Translate(-125, 125); ///???
+	Transform(srcImage, dstImage, affineTf, interpolator);
+	return 1;
 }
 
 int GeometricTransformer::Scale(const Mat & srcImage, Mat & dstImage, float sx, float sy, PixelInterpolate * interpolator)
 {
+	//Khởi tạo đối tượng affineTf và gọi phương thức Scale
+	AffineTransform * affineTf = new AffineTransform();
+	affineTf->Scale(1.0f / sx, 1.0f / sy);
 
-	return 0;
+	//Khởi tạo ảnh đích có kích thước và type giống ảnh nguồn
+	dstImage.create(round(srcImage.rows * sx), round(srcImage.cols * sy), srcImage.type());
+
+	return Transform(srcImage, dstImage, affineTf, interpolator);
 }
 
 GeometricTransformer::GeometricTransformer()
--- Labs/Lab02/Lab02.cpp
+++ Labs/Lab02/Lab02.cpp
@@ -1,4 +1,4 @@
-#include "GeometricTransformer.h"
+﻿#include "GeometricTransformer.h"
 #include <string>
 #include <iostream>
 
@@ -8,25 +8,65 @@ int main(int argc, char * argv[])
 {
 	//srcImg: anh nguon - dstImg: anh dich
 	Mat srcImg, dstImg;
+	//Khởi tạo lớp GeometricTransformer
 	GeometricTransformer GTf;
-
+	PixelInterpolate *pixInterpolate;
 	
 	string Command(argv[1]), Interpolate(argv[2]), fileImg(argv[3]);
+
+	int result = 0; //biến lưu kết quả trả về sau khi xử lý
 
 	srcImg = imread(fileImg, -1);
 
 	if (Command == "--zoom") {
-		float factor = atof(argv[4]);
+		float factor = atof(argv[4]); //Factor là hệ số zoom
 		if (Interpolate == "--bl")
-			GTf.Scale(srcImg, dstImg, factor, factor, BilinearInterpolate());
+		{
+			pixInterpolate = new BilinearInterpolate();
+			result = GTf.Scale(srcImg, dstImg, factor, factor, pixInterpolate);
+		}
+		else
+		{
+			pixInterpolate = new NearestNeighborInterpolate();
+			result = GTf.Scale(srcImg, dstImg, factor, factor, pixInterpolate);
+		}
 	}
 	else if (Command == "--rotate") {
-
+		float angle = atof(argv[4]); //angle là góc xoay
+		if (Interpolate == "--bl")
+		{
+			pixInterpolate = new BilinearInterpolate();
+			result = GTf.RotateKeepImage(srcImg, dstImg, angle, pixInterpolate);
+		}
+		else
+		{
+			pixInterpolate = new NearestNeighborInterpolate();
+			result = GTf.RotateKeepImage(srcImg, dstImg, angle, pixInterpolate);
+		}
 	}
 	else if (Command == "--rotateN") {
-
+		float angle = atof(argv[4]); //angle là góc quay
+		if (Interpolate == "--bl")
+		{
+			pixInterpolate = new BilinearInterpolate();
+			result = GTf.RotateUnkeepImage(srcImg, dstImg, angle, pixInterpolate);
+		}
+		else
+		{
+			pixInterpolate = new NearestNeighborInterpolate();
+			result = GTf.RotateUnkeepImage(srcImg, dstImg, angle, pixInterpolate);
+		}
 	}
+	Mat temp;
 	
+	imshow("Source Image", srcImg);
+	cout << srcImg.cols << " " << srcImg.rows << endl;
+	if (result)
+	{
+		imshow("Destination Image", dstImg);
+		cout << dstImg.cols << " " << dstImg.rows << endl;
+	}
+
 	waitKey(0);
 	return 0;
 }
