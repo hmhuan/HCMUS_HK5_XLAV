e86a3b78b6177ff855f415bb4ccb43660d2e4cd2
hmhuan <minhhuanhuynh289@gmail.com>
2018-12-06 22:27:32 +0700

    Initialize Lab06: SVD compress image

---
+++ Labs/Lab06/Lab06.cpp
@@ -0,0 +1,46 @@
+﻿#include "SVD.h"
+
+int main(int argc, char * argv[])
+{
+	Mat srcImage, cvtImage, dstImage, w, u, vt;
+	string Command(argv[1]), InputPath(argv[2]), OutputPath(argv[3]);
+	int result = 0, type;
+
+	//Read Image with type gray 
+	//srcImage = imread("2.png", CV_8UC1);
+	////Convert Image type to CV_32F
+	//srcImage.convertTo(cvtImage, CV_32F);
+	//int height = srcImage.rows, width = srcImage.cols;
+	//cout << srcImage.type() << endl;
+	//SVD svd(srcImage);	
+	//cout << height << " " << width << "\n\n";
+
+	//cout << svd.u.rows << " " << svd.u.cols << endl;
+	//cout << svd.vt.rows << " " << svd.vt.cols << endl;
+	////cout << svd.w << "\n";
+
+	srcImage = imread(InputPath, -1);
+	if (!srcImage.empty())
+		imshow("Original Image", srcImage);
+	if (Command == "--compress")
+	{
+		int k = atoi(argv[4]);
+		type = 1;
+		result = Compress(srcImage, dstImage, k);
+	}
+	else if (Command == "--decompress")
+	{
+		type = 2;
+		result = Decompress(srcImage, dstImage);
+	}
+
+	if (result)
+	{
+		//Nếu decompress hiển thị ảnh giải nén
+		if (type == 2)
+			imshow("Decompress Image", dstImage);
+		imwrite(OutputPath, dstImage);
+	}
+	waitKey(0);
+	return 0;
+}
---
+++ Labs/Lab06/SVD.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include "opencv2\opencv.hpp"
+#include "opencv2\highgui\highgui.hpp"
+
+using namespace std;
+using namespace cv;
+
+int Compress(const Mat& srcImage, Mat &dstImage, int k)
+{
+	if (srcImage.empty())
+		return 0;
+	Mat cvtImage;
+	cvtColor(srcImage, cvtImage, CV_BGR2GRAY);
+	imshow("Gray", cvtImage);
+	cvtImage.convertTo(cvtImage, CV_32FC1);
+	cout << cvtImage.type() << endl;
+	//Tạo svd
+	SVD svd(cvtImage);
+	cout << "SVD done!!!" << endl;
+	cout << svd.u.rows << " " << svd.u.cols << endl;
+	cout << svd.w.rows << " " << svd.w.cols << endl;
+	cout << svd.vt.rows << " " << svd.vt.cols << endl;
+	//Thực hiện nén 
+	if (dstImage.empty())
+		return 0;
+	return 1;
+}
+
+int Decompress(const Mat& srcImage, Mat &dstImage)
+{
+	if (srcImage.empty())
+		return 0;
+
+
+
+
+	if (dstImage.empty())
+		return 0;
+	return 1;
+}
